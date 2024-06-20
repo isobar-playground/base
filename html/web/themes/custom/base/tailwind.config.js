6919d9c4747ebaccecb7241fa8604c0d8c4f9b74
Marcin Maruszewski <marcin.maruszewski@isobar.com>
2024-06-21 00:12:25 +0200

    Working with tailwind components.

--- html/web/themes/custom/base/tailwind.config.js
+++ html/web/themes/custom/base/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './{templates,components}/**/*.twig',
+    './{templates,components,stories}/**/*.twig',
   ],
   theme: {
     extend: {},
