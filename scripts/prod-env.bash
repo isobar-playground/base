b984f46ab6703cfd383a5baae202eb633facbbf1
Marcin Maruszewski <marcin.maruszewski@isobar.com>
2024-07-08 23:25:06 +0200

    Add bash script for managing images tags without -dev- flag.

---
+++ scripts/prod-env.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+while IFS= read -r line; do
+    [[ $line == \#* ]] && continue
+    if [[ $line == *"-dev-"* ]]; then
+        key=$(echo "$line" | cut -d'=' -f1)
+        new_value=$(echo "$line" | sed 's/-dev-/-/')
+        if grep -q "^$key=" .env.local; then
+            sed -i "s|^$key=.*|$new_value|" .env.local
+        else
+            echo "$new_value" >> .env.local
+        fi
+    fi
+done < .env
